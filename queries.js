9b13e9dda4f8c81aa1f1b9273b8d5795893d84a7
Thomas Clukies <Thomas@Thomass-MacBook-Pro.local>
2018-08-28 16:06:34 -0600

    show book and author by id routes completed

--- queries.js
+++ queries.js
@@ -28,5 +28,37 @@ module.exports = {
                 portrait_url: "author.portrait_url",
                 biography: "author.biography"
             });
+    },
+    listBooksByID(id) {
+        return database("authors_books")
+            .join("book", "authors_books.book_id", "=", "book.id")
+            .join("author", "authors_books.author_id", "=", "author.id")
+            .select({
+                book_id: "book.id",
+                title: "book.title",
+                first_name: "author.first_name",
+                last_name: "author.last_name",
+                genre: "book.genre",
+                cover_url: "book.cover_url",
+                synopsis: "book.synopsis"
+            })
+            .where("authors_books.book_id", id)
+            .first();
+    },
+    listAuthorsByID(id) {
+        return database("authors_books")
+            .join("book", "authors_books.book_id", "=", "book.id")
+            .join("author", "authors_books.author_id", "=", "author.id")
+            .select({
+                author_id: "author.id",
+                title: "book.title",
+                first_name: "author.first_name",
+                last_name: "author.last_name",
+                genre: "book.genre",
+                portrait_url: "author.portrait_url",
+                biography: "author.biography"
+            })
+            .where("authors_books.author_id", id)
+            .first();
     }
 };
